2e848baa7f65d56f8fa86404f266071adbe37359
Teck Yong Lee <115521798+teckyonglee@users.noreply.github.com>
2022-10-11 13:48:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import streamlit
 
-
 streamlit.title("My Parents New Healthy Diner")
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥦 Kale, Spinach & Rocket Smootie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭Build Your Own Fruit Smootie🥝🍇')
+
