f79433f96dd396af84089710b6e9c2b20f1f74d5
Teck Yong Lee <115521798+teckyonglee@users.noreply.github.com>
2022-10-15 15:16:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ streamlit.header("View Our Fruit List - Add Your Favourites!")
 #Snowflake-related functions
 def getload_list():
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("select* fron fruit_load_list")
+        my_cur.execute("select* from fruit_load_list")
         return my_cur.fetchall()
     
 # Add a button to load the fruit    
@@ -62,7 +62,7 @@ if streamlit.button('Get Fruit List'):
 #Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values('add_my_fruit')")
+        my_cur.execute("insert into fruit_load_list values('" + add_my_fruit +')")
         return "Thanks for adding " + new_fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
